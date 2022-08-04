6187ee839e9a66e365423e4ed6505e4e7d78ac73
Philip Chu <technicat@users.noreply.github.com>
2022-08-04 11:40:38 -0700

    Update size.scm

--- size.scm
+++ size.scm
@@ -8,6 +8,7 @@
   (let-args (cdr args)
       ((h "h|help")
         (f "f|file=s")
+        (i "i|file")
         (t "t|type=s")
        . restargs
       )
@@ -19,20 +20,21 @@
             (print "Specify a file type (suffix) to filter.")
             (print "Examples:")
             (print "size.scm -h")
+            (print "size.scm -i")
             (print "size.scm -f countlines.scm")
             (print "size.scm -t scm"))
         (let ((count 
             (if f
                 (file-size f)
-                (count-current-directory t))))
+                (size-current-directory :type t :ignore-hidden i))))
             (print #"~count bytes")))))
 
-(define count-current-directory
-    (lambda (type)
-        (count-directory (current-directory) type)))
+(define size-current-directory
+    (lambda (:key (type #f) (ignore-hidden #f))
+        (size-directory (current-directory) :type type :ignore-hidden ignore-hidden)))
 
-(define count-directory
-    (lambda (path type) 
+(define size-directory
+    (lambda (path :key (type #f) (ignore-hidden #f)) 
         (directory-fold path
             (lambda (file result)
                 (+ result 
@@ -40,5 +42,5 @@
             0
             :lister
             (lambda (dir seed)
-                (values (filter-dir dir :type type)
+                (values (filter-dir dir :type type :ignore-hidden ignore-hidden)
                     seed)))))
