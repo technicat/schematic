ac479ff0843c5f148bc6439d68bacc114209165d
Philip Chu <technicat@users.noreply.github.com>
2022-08-06 12:26:15 -0700

    sexp

--- sexp.scm
+++ sexp.scm
@@ -1,7 +1,6 @@
 #!/usr/local/bin/gosh
 
 (use gauche.parseopt)
-(use rfc.json)
 
 (include "lib/dir.scm")
 
@@ -10,30 +9,30 @@
       ((h "h|help" => (cut help (car args)))
        (d "d|dot-files")
         (f "f|file=s")
-          (t "t|type=s" "json")
+          (t "t|type=s" "scm")
          (v "v|verbose")
        . restargs
       )
     (if (not h)
         (if f
             (json-file f p)
-            (let ((count (json-dir (current-directory) :type t :dot-files d :verbose v)))
-                (print #"Checked ~count JSON files"))))))
+            (let ((count (sexp-dir (current-directory) :type t :dot-files d :verbose v)))
+                (print #"Checked ~count files"))))))
 
 (define help
     (lambda (file)
-        (print "Validate JSON in file or current directory (and below).")
+        (print "Validate s expressions in file or current directory (and below).")
         (dir-help)
 ))
 
 ; todo - should return json results
-(define json-dir
+(define sexp-dir
     (lambda (path :rest args) 
         (apply dir-info path args)
         (print #"checking all files in ~path")
         (directory-fold path
             (lambda (file result)
-                    (json-file file)
+                    (sexp-file file)
                     (+ 1 result))
             0
             :lister
@@ -41,12 +40,19 @@
                 (values (apply filter-dir dir args)
                     seed)))))
 
-(define json-file
+(define sexp-file
     (lambda (file)
-        (guard (e (else (print #"JSON error in ~file")
+        (guard (e (else (print #"error in ~file")
                         (print (condition-message e))
                         #\f))
-            (let ((exp (call-with-input-file file parse-json)))
-              ;  (if verbose (print exp))
+            (let ((exp (call-with-input-file file sexp-input)))
                 exp))))
+
+
+(define sexp-input
+    (lambda (p)
+        (let ((x (read p)))
+                (if (eof-object? x)
+                    '()
+                     x))))
 
