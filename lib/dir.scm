3c70f42a7fe65d3b5c4f75d74c8de2611ac7e26f
Philip Chu <technicat@users.noreply.github.com>
2022-08-13 12:05:40 -0700

    Update dir.scm

--- lib/dir.scm
+++ lib/dir.scm
@@ -3,45 +3,41 @@
 (use file.util)
 
 ; todo - add follow-symlinks option
-(define dir-help
- (lambda ()
-  (print "-h : show this doc")
-  (print "-d : process dot (hidden) files")
-  (print "-t : file extension to filter for, e.g. scm, json...")
-  (print "-v : print progress")
-  ))
+(define (dir-help)
+ (print "-h : show this doc")
+ (print "-d : process dot (hidden) files")
+ (print "-t : file extension to filter for, e.g. scm, json...")
+ (print "-v : print progress")
+ )
 
-(define dir-info
- (lambda (path :key (type #f) (dot-files #f) (verbose #f))
-  (print #"checking all files in ~path")
-  (if type
-   (print #"with extension ~type"))
-  (if dot-files
-   (print "including dot (hidden) files")
-   (print "ignoring dot (hidden) files"))
-  (if verbose
-   (print "verbose is on")
-   (print "verbose is off"))))
+(define (dir-info path :key (type #f) (dot-files #f) (verbose #f))
+ (print #"checking all files in ~path")
+ (if type
+  (print #"with extension ~type"))
+ (if dot-files
+  (print "including dot (hidden) files")
+  (print "ignoring dot (hidden) files"))
+ (if verbose
+  (print "verbose is on")
+  (print "verbose is off")))
 
 ; todo - make type a list
-(define ignore-file?
- (lambda (file :key (type #f) (dot-files #f) (verbose #f))
-  (let-values (((dir name ext) (decompose-path file)))
-   (let ((ignore (or
-                  (and (not dot-files) (eq? (string-ref name 0) #\.))
-                  (and type
-                   (file-is-regular? file)
-                   (not (equal? ext type))))))
-    (if (and verbose ignore)
-     (print #"Ignoring ~file")
-     )
-    ignore))))
+(define (ignore-file? file :key (type #f) (dot-files #f) (verbose #f))
+ (let-values (((dir name ext) (decompose-path file)))
+  (let ((ignore (or
+                 (and (not dot-files) (eq? (string-ref name 0) #\.))
+                 (and type
+                  (file-is-regular? file)
+                  (not (equal? ext type))))))
+   (if (and verbose ignore)
+    (print #"Ignoring ~file")
+    )
+   ignore)))
 
-(define filter-dir
- (lambda (dir :rest args)
-  (remove
-   (lambda (file)
-    (apply ignore-file? file args))
-   (directory-list dir :add-path? #t :children? #t))))
+(define (filter-dir dir :rest args)
+ (remove
+  (lambda (file)
+   (apply ignore-file? file args))
+  (directory-list dir :add-path? #t :children? #t)))
 
 
