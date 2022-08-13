ccf5a55150cc6c39dc5eefe9fad731400edabff2
Philip Chu <technicat@users.noreply.github.com>
2022-08-13 12:03:41 -0700

    Update indent.scm

--- indent.scm
+++ indent.scm
@@ -28,117 +28,109 @@
   (dir-help)
   ))
 
-(define indent-dir
- (lambda (path :rest args)
-  (apply dir-info path args)
-  (directory-fold path
-   (lambda (file result)
-    (let ((perm (slot-ref (sys-stat file) 'perm)))
-     (if (indent-file file)
-      (begin
-       (sys-chmod file perm)
-       (+ 1 result))
-      result)))
-   0
-   :lister
-   (lambda (dir seed)
-    (values (apply filter-dir dir args)
-     seed)))))
+(define (indent-dir path :rest args)
+ (apply dir-info path args)
+ (directory-fold path
+  (lambda (file result)
+   (let ((perm (slot-ref (sys-stat file) 'perm)))
+    (if (indent-file file)
+     (begin
+      (sys-chmod file perm)
+      (+ 1 result))
+     result)))
+  0
+  :lister
+  (lambda (dir seed)
+   (values (apply filter-dir dir args)
+    seed))))
 
-(define indent-file
- (lambda (file)
-  (guard (e (else
-             (print #"Error processing ~file")
-             (print (condition-message e))
-             #f))
-   (file-filter-fold indent-fold '() :input file :output file :temporary-file #t))))
+(define (indent-file file)
+ (guard (e (else
+            (print #"Error processing ~file")
+            (print (condition-message e))
+            #f))
+  (file-filter-fold indent-fold '() :input file :output file :temporary-file #t)))
 
-(define indent-fold
- (lambda (line columns out)
-  (let ((new (string-trim-both line)))
-   (if (> (string-length new) 0)
-    (let ((column (if (null? columns)
-                   0
-                   (car columns))))
-     (write-string
-      (string-pad new (+ column
-                       (string-length new)))
-      out)))
-   (newline out)
-   (if (or
-        (= (string-length new) 0)
-        (eq? (string-ref new 0) #\;))
-    columns
-    (new-columns new columns)))))
+(define (indent-fold line columns out)
+ (let ((new (string-trim-both line)))
+  (if (> (string-length new) 0)
+   (let ((column (if (null? columns)
+                  0
+                  (car columns))))
+    (write-string
+     (string-pad new (+ column
+                      (string-length new)))
+     out)))
+  (newline out)
+  (if (or
+       (= (string-length new) 0)
+       (eq? (string-ref new 0) #\;))
+   columns
+   (new-columns new columns))))
 
-(define new-columns
- (lambda (s columns)
-  (let f ((chars (string->list s))
-          (col (if (null? columns)
-                0
-                (car columns)))
-          (cols columns))
-   (if (null? chars)
-    cols
-    (case (car chars)
-     ((#\()
-      (f (cdr chars) (+ 1 col) (cons (+ 1 col) cols)))
-     ((#\))
-      (f (cdr chars) (+ 1 col) (if (null? cols)
-                                cols
-                                (cdr cols))))
-     ((#\#)
-      (let-values (((chars col) (hash (cdr chars) (+ 1 col))))
-       (f chars col cols)))
-     ((#\")
-      (let-values (((chars col) (quotation (cdr chars) (+ 1 col))))
-       (f chars col cols)))
-     ((#\;)
-      cols)
-     (else
-      (f (cdr chars) (+ 1 col) cols)))))))
-
-(define hash
- (lambda (chars col)
+(define (new-columns s columns)
+ (let f ((chars (string->list s))
+         (col (if (null? columns)
+               0
+               (car columns)))
+         (cols columns))
   (if (null? chars)
-   (values chars col)
+   cols
    (case (car chars)
-    ((#\\) ; char
-     (escape (cdr chars) (+ 1 col)))
-    ((#\/) ; regexp
-     (regexp (cdr chars) (+ 1 col)))
+    ((#\()
+     (f (cdr chars) (+ 1 col) (cons (+ 1 col) cols)))
+    ((#\))
+     (f (cdr chars) (+ 1 col) (if (null? cols)
+                               cols
+                               (cdr cols))))
+    ((#\#)
+     (let-values (((chars col) (hash (cdr chars) (+ 1 col))))
+      (f chars col cols)))
+    ((#\")
+     (let-values (((chars col) (quotation (cdr chars) (+ 1 col))))
+      (f chars col cols)))
+    ((#\;)
+     cols)
     (else
-     (values chars col))))))
+     (f (cdr chars) (+ 1 col) cols))))))
+
+(define (hash chars col)
+ (if (null? chars)
+  (values chars col)
+  (case (car chars)
+   ((#\\) ; char
+    (escape (cdr chars) (+ 1 col)))
+   ((#\/) ; regexp
+    (regexp (cdr chars) (+ 1 col)))
+   (else
+    (values chars col)))))
 
 ; skip to end of escape
 ; todo - character names
-(define escape
- (lambda (chars col)
-  (if (null? chars)
-   (values chars col)
-   (values (cdr chars) (+ 1 col)))))
+(define (escape chars col)
+ (if (null? chars)
+  (values chars col)
+  (values (cdr chars) (+ 1 col))))
 
 ; skip to end of quote
-(define quotation
- (lambda (chars col)
-  (if (null? chars)
-   (values chars col)
-   (case (car chars)
-    ((#\\) ; char
-     (let-values (((chars col)
-                   (escape (cdr chars) (+ 1 col))))
-      (quotation chars col)))
-    ((#\")
-     (values (cdr chars) (+ 1 col)))
-    (else
-     (quotation (cdr chars) (+ 1 col)))))))
+(define (quotation chars col)
+ (if (null? chars)
+  (values chars col)
+  (case (car chars)
+   ((#\\) ; char
+    (let-values (((chars col)
+                  (escape (cdr chars) (+ 1 col))))
+     (quotation chars col)))
+   ((#\")
+    (values (cdr chars) (+ 1 col)))
+   (else
+    (quotation (cdr chars) (+ 1 col))))))
 
 ; skip to end of regexp
-(define regexp
- (lambda (chars col)
-  (if (null? chars)
-   (values chars col)
-   ; todo - check for escaped slash
-   (if (eqv? #\/ (car chars))
-    (values (cdr chars) (+ 1 col))
-    (quotation (cdr chars) (+ 1 col))))))
+(define (regexp chars col)
+ (if (null? chars)
+  (values chars col)
+  ; todo - check for escaped slash
+  (if (eqv? #\/ (car chars))
+   (values (cdr chars) (+ 1 col))
+   (quotation (cdr chars) (+ 1 col)))))
