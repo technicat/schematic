939b307ba271f67cf4a35fbdf7918f74726c165a
Philip Chu <technicat@users.noreply.github.com>
2022-08-13 19:01:31 -0700

    Update jsonfix.scm

--- jsonfix.scm
+++ jsonfix.scm
@@ -15,10 +15,9 @@
    . restargs)
   (if (not h)
    (if f
-    (let ((count (length (json-file f))))
-     (print #"Found ~count JSON objects"))
+    (json-file f)
     (let ((count (json-dir (current-directory) :type t :dot-files d :verbose v)))
-     (print #"Checked ~count JSON files"))))))
+     (print #"Rewrote ~count JSON files"))))))
 
 (define (help file)
  (print "Validate JSON in file or current directory (and below).")
@@ -30,19 +29,25 @@
   (apply dir-info path args)
   (directory-fold path
    (lambda (file result)
-    (json-file file)
-    (+ 1 result))
-   0
-   :lister
-   (lambda (dir seed)
-    (values (apply filter-dir dir args)
-     seed)))))
+    (guard (e (else (print #"JSON error in ~file")
+               (print (condition-message e))
+               result))
+     (json-file file)
+     (+ 1 result))
+    0
+    :lister
+    (lambda (dir seed)
+     (values (apply filter-dir dir args)
+      seed)))))
 
-(define json-file
- (lambda (file)
-  (guard (e (else (print #"JSON error in ~file")
-             (print (condition-message e))
-             #\f))
-   (let ((exp (call-with-input-file file parse-json*)))
-    exp))))
+ (define (json-file file)
+  (file-filter json-filter
+   :input file :output file
+   :temporary-file #t
+   :leave-unchanged #t))
+
+ (define (json-filter in out)
+   (let f ((objs (parse-json* in)))
+    (if (not (null? objs))
+        (construct-json (car objs) out))))
 
